9a7fd15c56b643f3707c6d50f828982ca9abe896
Willi Schinmeyer <willi@schinmeyer.de>
2011-12-16 21:32:32 +0100

    Started basic project. Does nothing yet.

---
+++ renaissance/addons/ld22/modinfo.lua
@@ -0,0 +1,7 @@
+name = "Ludum Dare 22 Project"
+version = 1
+displayVersion = "pre-alpha"
+author = "Mr. Wonko"
+description = "Base project I'll probably be using for Ludum Dare 22"
+website = "http://www.mrwonko.de"
+email = "willi@schinmeyer.de"
--- renaissance/bin/renaissance.sh
+++ renaissance/bin/renaissance.sh
@@ -2,4 +2,4 @@
 
 FILEDIR=$(dirname $0)
 export LD_LIBRARY_PATH=$FILEDIR/
-$FILEDIR/03-main $@
+$FILEDIR/renaissance $@
