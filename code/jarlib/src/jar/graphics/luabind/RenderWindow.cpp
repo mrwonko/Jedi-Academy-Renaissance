8b238191d8c0b3417775918ec5311ace2e543c57
Willi Schinmeyer <willi@schinmeyer.de>
2012-04-11 21:55:01 +0200

    Updated jar::Renderwindow luabind export to SFML 2.0-rc

--- code/jarlib/src/jar/graphics/luabind/RenderWindow.cpp
+++ code/jarlib/src/jar/graphics/luabind/RenderWindow.cpp
@@ -13,31 +13,24 @@
 namespace jar
 {
 
-static TimeType GetFrameTime(const jar::RenderWindow& window)
+namespace
 {
-    //on high fps rounding may always result in 0, so I need to keep track of the time lost through rounding.
-    static float dif = 0;
+    void RenderWindowSetIcon(jar::RenderWindow& window, const sf::Texture& texture)
+    {
+        sf::Image img(texture.copyToImage());
+        window.setIcon(img.getSize().x, img.getSize().y, img.getPixelsPtr());
+    }
 
-    float time = window.GetFrameTime() * TimeTypesPerSecond;
+    void SetCursorPosition(const RenderWindow& window, const sf::Vector2i& position)
+    {
+        //TODO/FIXME: what coordinate system is used here?
+        sf::Mouse::setPosition(position, window);
+    }
 
-    time += dif;
-
-    TimeType roundedTime = jar::Helpers::Round(time);
-
-    dif = time - roundedTime;
-
-    return roundedTime;
-}
-
-static void RenderWindowSetIcon(jar::RenderWindow& window, const sf::Texture& texture)
-{
-    sf::Image img(texture.CopyToImage());
-    window.SetIcon(img.GetWidth(), img.GetHeight(), img.GetPixelsPtr());
-}
-
-void SetCursorPosition(const RenderWindow& window, const sf::Vector2i& position)
-{
-    sf::Mouse::SetPosition(position, window);
+    void RenderWindowDraw(RenderWindow& window, const sf::Drawable& drawable)
+    {
+        window.draw(drawable);
+    }
 }
 
 void RenderWindow::Luabind(lua_State* L)
@@ -46,11 +39,11 @@ void RenderWindow::Luabind(lua_State* L)
     [
         luabind::class_<RenderWindow>("RenderWindow")
             .def(luabind::constructor<const unsigned int, const unsigned int, const std::string&, const bool, const bool>())
-            .def("Clear", &jar::RenderWindow::Clear)
-            .def("Draw", (void(jar::RenderWindow::*)(const sf::Drawable&))&jar::RenderWindow::Draw)
-            .def("SetView", &jar::RenderWindow::SetView)
-            .def("GetView", &jar::RenderWindow::GetView)
-            .def("GetDefaultView", &jar::RenderWindow::GetDefaultView)
+            .def("Clear", &jar::RenderWindow::clear)
+            .def("Draw", RenderWindowDraw)
+            .def("SetView", &jar::RenderWindow::setView)
+            .def("GetView", &jar::RenderWindow::getView)
+            .def("GetDefaultView", &jar::RenderWindow::getDefaultView)
             //NOTE: no longer exists in SFML 2.0 - now there are SaveGLStates() and RestoreGLStates() - need to know what they do first.
             //.def("PreserveOpenGLStates", &jar::RenderWindow::PreserveOpenGLStates)
 
@@ -58,23 +51,20 @@ void RenderWindow::Luabind(lua_State* L)
             //NOTE: this either does not yet exist in 2.0 or I just haven't found it - ok found it, it's Image::CopyFromScreen() or sth
             //.def("Capture", &RenderWindow::Capture)
 
-            .def("Close", &jar::RenderWindow::Close)
-            .def("Display", &jar::RenderWindow::Display)
-            .def("EnableKeyRepeat", &jar::RenderWindow::EnableKeyRepeat)
-            .def("GetFrameTime", &jar::GetFrameTime)
-            .def("GetHeight", &jar::RenderWindow::GetHeight)
+            .def("Close", &jar::RenderWindow::close)
+            .def("Display", &jar::RenderWindow::display)
+            .def("SetKeyRepeatEnabled", &jar::RenderWindow::setKeyRepeatEnabled) //todo: create jar.Keyboard for this?
+            .def("GetSize", &jar::RenderWindow::getSize)
             //maybe later...?
             //.def("GetSettings", &jar::RenderWindow::GetSettings)
-            .def("GetWidth", &jar::RenderWindow::GetWidth)
-            .def("SetActive", &jar::RenderWindow::SetActive)
+            .def("SetActive", &jar::RenderWindow::setActive)
 //            .def("SetCursorPosition", &jar::RenderWindow::SetCursorPosition)
             .def("SetCursorPosition", &SetCursorPosition)
-            .def("SetFramerateLimit", &jar::RenderWindow::SetFramerateLimit)
+            .def("SetFramerateLimit", &jar::RenderWindow::setFramerateLimit)
             .def("SetIcon", &RenderWindowSetIcon)
-            .def("SetPosition", &jar::RenderWindow::SetPosition)
-            .def("SetSize", &jar::RenderWindow::SetSize)
-            .def("Show", &jar::RenderWindow::Show)
-            .def("ShowMouseCursor", &jar::RenderWindow::ShowMouseCursor)
+            .def("SetPosition", &jar::RenderWindow::setPosition)
+            .def("SetSize", &jar::RenderWindow::setSize)
+            .def("SetMouseCursorVisible", &jar::RenderWindow::setMouseCursorVisible)
             //NOTE: this either does not yet exist in 2.0 or I just haven't found it
             //.def("UseVerticalSync", &jar::RenderWindow::UseVerticalSync)
     ];
