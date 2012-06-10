134abd6d6565868bc444e8a81f967c5dc7bbe29b
Willi Schinmeyer <willi@schinmeyer.de>
2012-06-10 12:46:17 +0200

    Got rid of silly temporary g2 loading structure, using STL instead of manual memory management

--- code/jarlib/include/jar/graphics/g2Model.hpp
+++ code/jarlib/include/jar/graphics/g2Model.hpp
@@ -2,6 +2,7 @@
 #define JAR_G2_MODEL_HPP
 
 #include <string>
+#include <vector>
 #include "jar/core/FileSystem.hpp"
 
 namespace jar
@@ -9,58 +10,38 @@ namespace jar
 
 namespace g2
 {
-    /** \brief The Ghoul 2 Model (.glm) as it is stored in memory
+    // forward declaration of implementation specific details
+    struct ModelRenderInfo;
 
-        \note Will be preprocessed for rendering
+    /** \brief Animatable, renderable Ghoul 2 Model (.glm)
     **/
-    struct ModelFile
+    class Model
     {
-        static const int VERSION = 6;
-        static const char* IDENT;// Note: Offsets are always relative to the address of the struct they appear in.
+    public:
+        Model();
+        ~Model();
+        const bool LoadFromFile(const std::string& filename, std::string& out_error);
 
-        struct Header
-        {
-            const bool LoadFromFile(fs::File& file, std::string& out_error);
+        /** \brief Uploads this model to the GPU's vram for fast rendering.
+        **/
+        const bool UploadToGPU();
+        /** \brief Deletes this model from the GPU's vram.
+        **/
+        const bool DeleteFromGPU();
+        /** \brief Renders this model. Requirement: Has been uploaded to the GPU.
+        **/
+        const bool Render();
 
-            char ident[4]; //"2LGM"
-            int version;
-            char name[64];
-            char animName[64];
-            int animIndex; // to be used by code as it pleases
-
-            int numBones; // for compatibility checks
-
-            int numLODs;
-            int ofsLODs;
-
-            int numSurfaces;
-            int ofsSurfHierarchy;
-
-            int ofsEnd; // = file size
-        };
-
-        struct SurfaceHierarchyOffsets
-        {
-            SurfaceHierarchyOffsets() : offsets(NULL) {}
-            ~SurfaceHierarchyOffsets() { delete[] offsets; }
-            const bool LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces);
-
-            int* offsets;
-        };
-
+    private:
         struct SurfaceHierarchyEntry
         {
-            SurfaceHierarchyEntry() : childIndices(NULL) {}
-            ~SurfaceHierarchyEntry() { delete[] childIndices; }
             const bool LoadFromFile(fs::File& file, std::string& out_error);
 
-            char name[64];
+            std::string name;
             unsigned int flags;
-            char shader[64];
-            int shaderIndex; // to be used by code as it pleases
+            std::string shader;
             int parentIndex; // -1 if root
-            int numChildren;
-            int* childIndices; // numChildren entries
+            std::vector<int> childIndices; // numChildren entries
         };
 
         struct Triangle
@@ -73,65 +54,39 @@ namespace g2
         {
             const bool LoadFromFile(fs::File& file);
 
-            float normal[3];
-            float coordinate[3];
-            unsigned int compressedWeightInfo;
-            unsigned char BoneWeights[4];
+            float normal[3];                //12 byte
+            float coordinate[3];            //12 byte
+            float uv[2];                    // 8 Byte
+            unsigned char boneIndices[4];   // 4 byte
+            unsigned short boneWeights[4];  // 8 byte
+                                            //44 byte
         };
 
         struct Surface
         {
-            Surface() : boneReferences(NULL), triangles(NULL), vertices(NULL) {}
-            ~Surface() { delete[] boneReferences; delete[] triangles; delete[] vertices; }
             const bool LoadFromFile(fs::File& file, std::string& out_error);
 
-            int ident; // to be used by code as it pleases
             int index;
-            int ofsHeader; // negative number pointing back to header
-            
-            int numVerts;
-            int ofsVerts;
 
-            int numTriangles;
-            int ofsTriangles;
-
-            int numBoneReferences;
-            int ofsBoneReferences;
-
-            int* boneReferences;
-            Triangle* triangles;
-            Vertex* vertices;
+            std::vector<int> boneReferences;
+            std::vector<Triangle> triangles;
+            std::vector<Vertex> vertices;
         };
 
         struct LOD
         {
-            LOD() : surfaces(NULL) {}
-            ~LOD() { delete[] surfaces; }
             const bool LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces);
 
-            int ofsEnd;
-            Surface* surfaces;
+            std::vector<Surface> surfaces;
         };
 
-        Header header;
-        SurfaceHierarchyOffsets hierarchyOffsets;
-        SurfaceHierarchyEntry* surfaceHierarchy; //mHeader.numSurfaces entries
-        LOD* LODs; //mHeader.numLODs entries
-
-        ModelFile();
-        ~ModelFile();
-        const bool LoadFromFile(const std::string& filename, std::string& out_error);
-    };
-
-    /** \brief Animatable model that can be rendered
-    **/
-
-    class Model
-    {
-    public:
-        Model();
-
-        const bool LoadFromFile(const std::string& filename, std::string& out_error);
+        std::string mName;
+        std::string mAnimName;
+        unsigned int mNumBones;
+        std::vector<SurfaceHierarchyEntry> mSurfaceHierarchy; //mHeader.numSurfaces entries
+        std::vector<SurfaceHierarchyEntry*> mRootSurfaces;
+        std::vector<LOD> mLODs; //mHeader.numLODs entries
+        ModelRenderInfo* mRenderInfo;
     };
 }
 }
--- code/jarlib/src/jar/graphics/g2Model.cpp
+++ code/jarlib/src/jar/graphics/g2Model.cpp
@@ -1,14 +1,81 @@
 #include "jar/graphics/g2Model.hpp"
 #include "jar/core/Helpers.hpp"
 #include <cassert>
+#include <SFML/OpenGL.hpp>
 
 namespace jar
 {
 namespace g2
 {
-    const char* ModelFile::IDENT = "2LGM";
 
-    const bool ModelFile::Header::LoadFromFile(fs::File& file, std::string& out_error)
+    // == OpenGL Specific rendering information ==
+    struct ModelRenderInfo
+    {
+        GLuint vertexVBOIndex;
+        GLuint triangleVBOIndex;
+    };
+
+    //
+
+    Model::Model() : mRenderInfo(new ModelRenderInfo)
+    {
+    }
+    
+    Model::~Model()
+    {
+        delete mRenderInfo;
+    }
+
+    // == File Format specific structures ==
+    static const int VERSION = 6;
+    static const char* IDENT = "2LGM";
+
+    // Note: Offsets are always relative to the address of the struct they appear in.
+    struct ModelHeader
+    {
+        const bool LoadFromFile(fs::File& file, std::string& out_error);
+
+        char ident[4]; //"2LGM"
+        int version;
+        char name[64];
+        char animName[64];
+        int animIndex; // to be used by code as it pleases
+
+        int numBones; // for compatibility checks
+
+        int numLODs;
+        int ofsLODs;
+
+        int numSurfaces;
+        int ofsSurfHierarchy;
+
+        int ofsEnd; // = file size
+    };
+
+    struct ModelSurfaceHierarchyOffsets
+    {
+        const bool LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces);
+
+        std::vector<int> offsets;
+    };
+
+    // == Helper functions ==
+
+    // Quake style char[64] string (need not end in NULL) to std::string
+    static std::string QuakeStringToString(const char* const str)
+    {
+        std::string result(str, 64);
+        std::string::size_type end = result.find_first_of('\0');
+        if(end != std::string::npos)
+        {
+            result.erase(end);
+        }
+        return result;
+    }
+
+    // == File Loading Functions ==
+
+    const bool ModelHeader::LoadFromFile(fs::File& file, std::string& out_error)
     {
         out_error = "Could not read file header!";
         if(fs::Read(file, ident, sizeof(char), 4) != 4) return false;
@@ -39,36 +106,47 @@ namespace g2
         if(numBones < 0 || numLODs < 0 || numSurfaces < 0)
         {
             out_error = "Invalid values.";
+            return false;
         }
 
         return true;
     }
 
-    const bool ModelFile::SurfaceHierarchyOffsets::LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces)
+    const bool ModelSurfaceHierarchyOffsets::LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces)
     {
-        offsets = new int[numSurfaces];
+        offsets.resize(numSurfaces);
         for(int i = 0; i < numSurfaces; ++i)
         {
             if(!fs::ReadInt(file, offsets[i]))
             {
                 out_error = "Error reading hierarchy offsets: " + fs::GetLastError();
+                return false;
             }
         }
         return true;
     }
 
-    const bool ModelFile::SurfaceHierarchyEntry::LoadFromFile(fs::File& file, std::string& out_error)
+    const bool Model::SurfaceHierarchyEntry::LoadFromFile(fs::File& file, std::string& out_error)
     {
         out_error = "Could not read surface hierarchy.";
-
-        if(fs::Read(file, name, sizeof(char), 64) != 64) return false;
+        
+        char qname[64];
+        if(fs::Read(file, qname, sizeof(char), 64) != 64) return false;
+        name = QuakeStringToString(qname);
+        
         if(!fs::ReadUnsignedInt(file, flags)) return false;
-        if(fs::Read(file, shader, sizeof(char), 64) != 64) return false;
+        
+        char qshader[64];
+        if(fs::Read(file, qshader, sizeof(char), 64) != 64) return false;
+        shader = QuakeStringToString(qshader);
+
+        int shaderIndex;
         if(!fs::ReadInt(file, shaderIndex)) return false;
         if(!fs::ReadInt(file, parentIndex)) return false;
+        
+        int numChildren;
         if(!fs::ReadInt(file, numChildren)) return false;
-        assert(childIndices == NULL);
-        childIndices = new int[numChildren];
+        childIndices.resize(numChildren);
         for(int i = 0; i < numChildren; ++i)
         {
             if(!fs::ReadInt(file, childIndices[i])) return false;
@@ -76,7 +154,7 @@ namespace g2
         return true;
     }
 
-    const bool ModelFile::Triangle::LoadFromFile(fs::File& file)
+    const bool Model::Triangle::LoadFromFile(fs::File& file)
     {
         if(!fs::ReadInt(file, indices[0])) return false;
         if(!fs::ReadInt(file, indices[1])) return false;
@@ -84,7 +162,7 @@ namespace g2
         return true;
     }
 
-    const bool ModelFile::Vertex::LoadFromFile(fs::File& file)
+    const bool Model::Vertex::LoadFromFile(fs::File& file)
     {
         if(!fs::ReadFloat(file, normal[0])) return false;
         if(!fs::ReadFloat(file, normal[1])) return false;
@@ -92,24 +170,68 @@ namespace g2
         if(!fs::ReadFloat(file, coordinate[0])) return false;
         if(!fs::ReadFloat(file, coordinate[1])) return false;
         if(!fs::ReadFloat(file, coordinate[2])) return false;
+
+        // weight & index are compressed:
+        unsigned int compressedWeightInfo;
         if(!fs::ReadUnsignedInt(file, compressedWeightInfo)) return false;
-        if(fs::Read(file, BoneWeights, sizeof(unsigned char), 4) != 4) return false;
+        unsigned char rawBoneWeights[4];
+        if(fs::Read(file, rawBoneWeights, sizeof(unsigned char), 4) != 4) return false;
+
+        // bit 30 and 31 are weight count
+        unsigned int numWeights = (compressedWeightInfo >> 30) + 1;
+
+        // indices are 5 bit each, index into the boneReferences (up to 32)
+        // weights are 10 bit each - 8 bit in rawBoneWeights, 2 in compressedWeightInfo
+        short totalWeight = 0;
+        static const short MAX_WEIGHT = (1 << 10) - 1;
+        for(unsigned int i = 0; i < 4; ++i)
+        {
+            //bottom 20 bits are 4 indices of 5 bit each
+            boneIndices[i] = (compressedWeightInfo >> (5 * i)) & ((1 << 5) - 1);
+            short weight = 0;
+            if(i < numWeights - 1)
+            {
+                // bit 20-27 are 2 bit weight overflow
+                // shift by 20 - 8 = 12 (8 because sizeof(rawBoneWeights[0]) == 8)
+                weight = rawBoneWeights[i] | ((compressedWeightInfo >> (12 + (2 * i))) & (0x300));
+                totalWeight += weight;
+            }
+            else if(i == numWeights - 1)
+            {
+                weight = MAX_WEIGHT - totalWeight;
+            }
+            else
+            {
+                boneIndices[i] = 0;
+            }
+            // shift weight so it uses the whole short range and can be properly normalized
+            boneWeights[i] = weight << (sizeof(unsigned short) * 8 - 10);
+        }
+
         return true;
     }
 
-    const bool ModelFile::Surface::LoadFromFile(fs::File& file, std::string& out_error)
+    const bool Model::Surface::LoadFromFile(fs::File& file, std::string& out_error)
     {
         const int baseOffset = fs::Tell(file);
 
         out_error = "Could not read surface.";
+        int ident; // used by original code
         if(!fs::ReadInt(file, ident)) return false;
         if(!fs::ReadInt(file, index)) return false;
+        int ofsHeader; // negative number pointing back to header
         if(!fs::ReadInt(file, ofsHeader)) return false;
+        int numVerts;
         if(!fs::ReadInt(file, numVerts)) return false;
+        int ofsVerts;
         if(!fs::ReadInt(file, ofsVerts)) return false;
+        int numTriangles;
         if(!fs::ReadInt(file, numTriangles)) return false;
+        int ofsTriangles;
         if(!fs::ReadInt(file, ofsTriangles)) return false;
+        int numBoneReferences;
         if(!fs::ReadInt(file, numBoneReferences)) return false;
+        int ofsBoneReferences;
         if(!fs::ReadInt(file, ofsBoneReferences)) return false;
 
         if(ofsHeader != -baseOffset)
@@ -124,11 +246,15 @@ namespace g2
             out_error = "Error seeking surface bone references: " + fs::GetLastError();
             return false;
         }
-        assert(boneReferences == NULL);
-        boneReferences = new int[numBoneReferences];
+        boneReferences.resize(numBoneReferences);
+        std::vector<int>::iterator end = boneReferences.end();
         for(int i = 0; i < numBoneReferences; ++i)
         {
-            if(!fs::ReadInt(file, boneReferences[i])) return false;
+            if(!fs::ReadInt(file, boneReferences[i]))
+            {
+                out_error = "Error reading bone references.";
+                return false;
+            }
         }
 
         //  Vertices
@@ -137,12 +263,21 @@ namespace g2
             out_error = "Error seeking surface vertices: " + fs::GetLastError();
             return false;
         }
-        assert(vertices == NULL);
-        vertices = new Vertex[numVerts];
-        const Vertex* const endVertices = vertices + numVerts;
-        for(Vertex* curVert = vertices; curVert < endVertices; ++curVert)
+        vertices.resize(numVerts);
+        std::vector<Vertex>::iterator endVertices = vertices.end();
+        for(std::vector<Vertex>::iterator curVert = vertices.begin(); curVert != endVertices; ++curVert)
         {
             if(!curVert->LoadFromFile(file))
+            {
+                out_error = "Could not read vertex.";
+                return false;
+            }
+        }
+        //UV Coordinates come after the other vertex information, for some reason.
+        for(std::vector<Vertex>::iterator curVert = vertices.begin(); curVert != endVertices; ++curVert)
+        {
+            if( !fs::ReadFloat(file, curVert->uv[0]) ||
+                !fs::ReadFloat(file, curVert->uv[1]) )
             {
                 out_error = "Could not read vertex.";
                 return false;
@@ -155,10 +290,9 @@ namespace g2
             out_error = "Error seeking surface triangles: " + fs::GetLastError();
             return false;
         }
-        assert(triangles == NULL);
-        triangles = new Triangle[numTriangles];
-        const Triangle* const triangleEnd = triangles + numTriangles;
-        for(Triangle* curTriangle = triangles; curTriangle < triangleEnd; ++curTriangle)
+        triangles.resize(numTriangles);
+        std::vector<Triangle>::iterator triangleEnd = triangles.end();
+        for(std::vector<Triangle>::iterator curTriangle = triangles.begin(); curTriangle != triangleEnd; ++curTriangle)
         {
             if(!curTriangle->LoadFromFile(file))
             {
@@ -171,9 +305,10 @@ namespace g2
     }
 
 
-    const bool ModelFile::LOD::LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces)
+    const bool Model::LOD::LoadFromFile(fs::File& file, std::string& out_error, const int numSurfaces)
     {
         const int baseOffset = fs::Tell(file);
+        int ofsEnd;
 
         if(!fs::ReadInt(file, ofsEnd))
         {
@@ -181,11 +316,12 @@ namespace g2
             return false;
         }
 
-        assert(surfaces == NULL);
-        surfaces = new Surface[numSurfaces];
-        for(Surface* curSurface = surfaces; curSurface != surfaces + numSurfaces; ++curSurface)
+        surfaces.resize(numSurfaces);
+
+        std::vector<Surface>::iterator end = surfaces.end();
+        for(std::vector<Surface>::iterator it= surfaces.begin(); it != end; ++it)
         {
-            if(!curSurface->LoadFromFile(file, out_error)) return false;
+            if(!it->LoadFromFile(file, out_error)) return false;
         }
         if(!fs::Seek(file, baseOffset + ofsEnd))
         {
@@ -195,21 +331,8 @@ namespace g2
         return true;
     }
 
-    ModelFile::ModelFile() :
-        surfaceHierarchy(NULL),
-        LODs(NULL)
+    const bool Model::LoadFromFile(const std::string& filename, std::string& out_error)
     {
-    }
-    
-    ModelFile::~ModelFile()
-    {
-        delete[] surfaceHierarchy;
-        delete LODs;
-    }
-
-    const bool ModelFile::LoadFromFile(const std::string& filename, std::string& out_error)
-    {
-
         //  open file
         fs::File file = fs::OpenRead(filename);
         if(!file)
@@ -219,19 +342,24 @@ namespace g2
         }
 
         //  read header
+        ModelHeader header;
         if(!header.LoadFromFile(file, out_error)) return false;
 
+        mName = QuakeStringToString(header.name);
+        mAnimName = QuakeStringToString(header.animName);
+
         //  read hierarchy
+        //read offsets
         if(!fs::Seek(file, header.ofsSurfHierarchy))
         {
             out_error = "Error seeking hierarchy offsets: " + fs::GetLastError();
             return false;
         }
-
+        ModelSurfaceHierarchyOffsets hierarchyOffsets;
         if(!hierarchyOffsets.LoadFromFile(file, out_error, header.numSurfaces)) return false;
 
-        assert(surfaceHierarchy == NULL);
-        surfaceHierarchy = new SurfaceHierarchyEntry[header.numSurfaces];
+        //read hierarchy
+        mSurfaceHierarchy.resize(header.numSurfaces);
         for(int index = 0; index < header.numSurfaces; ++index)
         {
             if(!fs::Seek(file, header.ofsSurfHierarchy + hierarchyOffsets.offsets[index]))
@@ -239,37 +367,34 @@ namespace g2
                 out_error = "Error seeking hierarchy offsets: " + fs::GetLastError();
                 return false;
             }
-            if(!surfaceHierarchy[index].LoadFromFile(file, out_error)) return false;
+            if(!mSurfaceHierarchy[index].LoadFromFile(file, out_error)) return false;
+            //remember root surfaces for hierarchical rendering
+            if(mSurfaceHierarchy[index].parentIndex == -1)
+            {
+                mRootSurfaces.push_back(&mSurfaceHierarchy[index]);
+            }
+        }
+        if(mRootSurfaces.size() == 0)
+        {
+            out_error = "Broken hierarchy!";
+            return false;
         }
 
-        // read LODs
+        //  read LODs
         if(!fs::Seek(file, header.ofsLODs))
         {
             out_error = "Error seeking LODs: " + fs::GetLastError();
             return false;
         }
-        assert(LODs == NULL);
-        LODs = new LOD[header.numLODs];
-        for(LOD* curLOD = LODs; curLOD != LODs + header.numLODs; ++curLOD)
+        mLODs.resize(header.numLODs);
+        std::vector<LOD>::iterator LODsEnd = mLODs.end();
+        for(std::vector<LOD>::iterator curLOD = mLODs.begin(); curLOD != LODsEnd; ++curLOD)
         {
             if(!curLOD->LoadFromFile(file, out_error, header.numSurfaces)) return false;
         }
 
         // since the file need not be in order, we can't sanity check against header.ofsEnd here
     
-        return true;
-    }
-
-    Model::Model()
-    {
-    }
-
-    const bool Model::LoadFromFile(const std::string& filename, std::string& out_error)
-    {
-        ModelFile modelFile;
-        if(!modelFile.LoadFromFile(filename, out_error)) return false;
-
-        //uncompress and convert to internal format etc. 
         return true;
     }
 }
